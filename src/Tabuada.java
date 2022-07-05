fc22733627e63a08f1a1714ea9c3bbc80ae3bb04
emmnesty <elyjhonatan@hotmail.com>
2022-07-05 16:23:04 -0300

    tabuada

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -9,7 +9,7 @@ public class Tabuada {
 			for (int j = 1; j <= 10; j++) {
 				int tabuada = j * i;
 
-				System.out.print(tabuada + " ");
+				System.out.print(tabuada + " "); 
 			}
 			System.out.println();
 
