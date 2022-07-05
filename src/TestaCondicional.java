ce15ae53ee4338cc0c19f586c77a4e251fe97944
emmnesty <elyjhonatan@hotmail.com>
2022-07-05 16:04:33 -0300

    tabuada

---
+++ src/TestaCondicional.java
@@ -0,0 +1,15 @@
+
+public class TestaCondicional {
+	public static void main(String[] args) {
+		System.out.println("Meu Teste de Condicional");
+
+		int idade = 18;
+
+		if (idade > 18) {
+			System.out.println("Já pode ir preso");
+		} else {
+			System.out.println("Foi por pouco mas fica ligeiro!");
+		}
+	}
+
+}
