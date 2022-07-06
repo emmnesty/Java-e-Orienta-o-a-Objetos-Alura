96829711f124826ad8667926e7ea1c5b9ecbab07
emmnesty <elyjhonatan@hotmail.com>
2022-07-06 13:48:02 -0300

    criando classe Cliente e praticando com objs

--- src/bitBank/MeuCliente.java
+++ src/bitBank/MeuCliente.java
@@ -2,20 +2,36 @@ package bitBank;
 
 public class MeuCliente {
 	public static void main(String[] args) {
-		Cliente cliente = new Cliente();
-		Conta conta = new Conta();
+		Cliente clienteJhonatan = new Cliente();
+		Conta contaDoJhonatan = new Conta();
 		
-		conta.saldo = 1000;
+		contaDoJhonatan.saldo = 1000;
 		
-		cliente.nome = "Jhonatan Maciel";
-		cliente.cpf = "222.222.222-22";
-		cliente.profissao = "Programador";
+		clienteJhonatan.nome = "Jhonatan Maciel";
+		clienteJhonatan.cpf = "222.222.222-22";
+		clienteJhonatan.profissao = "Programador";
 		
-		conta.titular = cliente;
+		contaDoJhonatan.titular = clienteJhonatan;
 		
-		System.out.println("Nome do titular: " + conta.titular.nome);
-		System.out.println("CPF do titular: " + conta.titular.cpf);
-		System.out.println("Nome da profissão: " + conta.titular.profissao);
+		System.out.println("Nome do titular: " + contaDoJhonatan.titular.nome);
+		System.out.println("CPF do titular: " + contaDoJhonatan.titular.cpf);
+		System.out.println("Nome da profissão: " + contaDoJhonatan.titular.profissao);
+		System.out.println();
+		
+		// Outra forma de fazer:
+		Cliente clienteAna = new Cliente();
+		Conta contaDaAna = new Conta();
+		
+		contaDaAna.saldo = 500;
+		
+		contaDaAna.titular = new Cliente();
+		
+		clienteAna.nome = "Ana Silva";
+		
+		System.out.println("Nome do Titular: " + clienteAna.nome);
+		System.out.println("Saldo: " + contaDaAna.saldo);
+		
+		
 		
 		
 	}
