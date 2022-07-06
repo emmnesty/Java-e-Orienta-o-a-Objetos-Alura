77be5ac8a801ed6634da38e9c6d77b190191bde8
emmnesty <elyjhonatan@hotmail.com>
2022-07-06 11:25:57 -0300

    Metodo transferencia

--- src/bitBank/Conta.java
+++ src/bitBank/Conta.java
@@ -1,20 +1,30 @@
 package bitBank;
 
 public class Conta {
-	 int agencia;
-	 int conta;
-	 double saldo;
-	 String nome;
-	 
-	 public void depositar (double valor) {
-		 this.saldo += valor;
-		 
-	 }
-	 
-	 public boolean sacar (double valor) {
-		 if(this.saldo >= valor) {
-			 this.saldo -= valor;
-			 return true;
-		 }else return false;
-	 }
+	int agencia;
+	int conta;
+	double saldo;
+	String nome;
+
+	public void depositar(double valor) {
+		this.saldo += valor;
+
+	}
+
+	public boolean sacar(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean transferir(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.sacar(valor);
+			destino.depositar(valor);
+			return true;
+		}
+		return false;
+	}
 }
--- src/bitBank/TesteAtributo.java
+++ src/bitBank/TesteAtributo.java
@@ -5,15 +5,29 @@ public class TesteAtributo {
 	public static void main(String[] args) {
 
 		Conta contaDoJhonatan = new Conta();
+		Conta contaDaAna = new Conta();
 
 		contaDoJhonatan.saldo = 100;
-		contaDoJhonatan.depositar(50);
+		contaDoJhonatan.depositar(1000);
+
+		//contaDoJhonatan.sacar(30);
+
+		System.out.println("Conta da Jhonatan: " + contaDoJhonatan.saldo);
 		
-		System.out.println(contaDoJhonatan.saldo);
+		//contaDaAna.depositar(2000);
+		System.out.println("Conta da Ana: " + contaDaAna.saldo);
 		
-		contaDoJhonatan.sacar(110);
 		
-		System.out.println(contaDoJhonatan.saldo);
+		boolean transferencia = contaDoJhonatan.transferir(200, contaDaAna);
+		
+		if (transferencia) {
+			System.out.println("Valor transferido com sucesso!");
+		} else {
+			System.out.println("Não há saldo suficiente!");
+		}
+		
+		System.out.println("Conta da Ana: " + contaDaAna.saldo);
+		
 
 	}
 
